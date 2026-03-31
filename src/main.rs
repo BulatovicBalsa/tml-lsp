6040564ed1ee0c413805326c65f492b046bc5c5c
BulatovicBalsa <baki.b.2k02@gmail.com>
2026-03-31 17:03:32 +0200

    add TML module and update file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod tml;
+
 use std::collections::HashMap;
 use std::sync::Arc;
 
---
+++ src/tml.rs
@@ -0,0 +1,12 @@
+mod tml;
+mod tml_actions;
+
+fn main() {
+    let snippet = r#"
+    fn test():
+        if any(true):
+            return
+        end
+    end
+    "#;
+}
